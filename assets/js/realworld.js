e6e56dfdb5d11f289a6b820827686ace234009b9
Fillip-M <fillipmosgaard@gmail.com>
2025-03-14 08:34:21 +0100

    done

--- assets/js/realworld.js
+++ assets/js/realworld.js
@@ -1,12 +1,9 @@
-
 /* du må både ændre koden her, og i din HTML, hvis det er nødvendigt.. */
-
 
 /*Opgave 1: skriv en validering af svar i koden herunder. resultatet skal vises i myAnsverFeedbackElement når bruger trykker på myAnsverButton .
 der et korrekte svar:
 nej 
 */
-
 
 const myAnsverInput = document.getElementById('answerOne');
 const myAnsverButton = document.getElementById('answerButton');
@@ -14,19 +11,32 @@ const myAnsverFeedbackElement = document.getElementById('answerFeedback');
 
 myAnsverButton.addEventListener('click', (e) => {
   e.preventDefault();
-  /* din kode her. Skriv en alert der skriver "du har svarret rigtigt" hvis svarret er true
-  og "du har svarret forkert" hvis svaret er false*/
   let myAnswer = myAnsverInput.value;
   console.log('Answer is: ' + myAnswer);
-});
 
-/*Opgave 2: udkommenter opgave 1, og omskriv nu din funktion til to korrekte svar:
+  /* if (myAnswer === 'false') {
+    myAnsverFeedbackElement.innerHTML = 'Du har svaret rigtigt!';
+  } else {
+    myAnsverFeedbackElement.innerHTML = 'Du har svaret forkert!';
+  } 
+      man skal gå ind i realworld.html og skrifte rundt på ja og nej value for at få det til at virke */
+
+  /* din kode her. Skriv en alert der skriver "du har svarret rigtigt" hvis svarret er true
+  og "du har svarret forkert" hvis svaret er false*/
+
+  /*Opgave 2: udkommenter opgave 1, og omskriv nu din funktion til to korrekte svar:
 nej
 ja i disney film
 */
 
+  /* if (myAnswer === 'false') {
+  myAnsverFeedbackElement.innerHTML = 'Du har svaret forkert!';
+} else {
+  myAnsverFeedbackElement.innerHTML = 'Du har svaret rigtigt!';
+}
+ */
 
-/*opgave 3: skriv en kode der valider formen med følgende elementer: (denne opgave er på extreme niveau) 
+  /*opgave 3: skriv en kode der valider formen med følgende elementer: (denne opgave er på extreme niveau) 
 
 - korrekt e-mail type. brug funktionen "validateEmail(minEmail) til at checke en string med den indtastede email
 - navnet skal være længere end 3 bogstaver, med realtime validering (brug keyup til at checke, og .lengt til at finde længden)
@@ -34,23 +44,18 @@ ja i disney film
  du må gerne bare consol.logge de forskellige tests i konsollen, men hvis du kan må du gerne lave feedback i elementet formValideringResult.
 */
 
+  /* helper functions*/
+  function validateEmail(email) {
+    var re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 
-
-
-
-
-
-
-/* helper functions*/
-function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-/* eks på kald:
+  /* eks på kald:
 if (validateEmail("test@example.com")) {
   console.log("Valid email address");
 } else {
   console.log("Invalid email address");
 }
 */
+});
